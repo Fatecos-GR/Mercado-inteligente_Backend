7ee74204af254dfe7245facac5af32479a0b3d27
pedro <pedrohenriquedez12@gmail.com>
2026-04-21 15:47:40 -0300

    chore: setup inicial do projeto e pipeline de CI

---
+++ src/main/java/br/edu/fatecgru/mercado_inteligente/MercadoInteligenteApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fatecgru.mercado_inteligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoInteligenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/fatecgru/mercado_inteligente/MercadoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fatecgru.mercado_inteligente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
