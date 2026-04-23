8184cfefff55457b6b718a63301960b7be021622
Renan Amaral <renanalvarado123@gmail.com>
2026-04-23 10:33:47 -0300

    Feat: Testar subida até o github e fluxo ded trabalho

--- src/main/java/br/edu/fatecgru/mercado_inteligente/MercadoInteligenteApplication.java
+++ src/main/java/br/edu/fatecgru/mercado_inteligente/MercadoInteligenteApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MercadoInteligenteApplication {
 
+	// Comentário Teste
 	public static void main(String[] args) {
 		SpringApplication.run(MercadoInteligenteApplication.class, args);
 	}
