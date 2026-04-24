efe267be4d58463c540efbca35ede01a1028a149
Renan Amaral <renanalvarado123@gmail.com>
2026-04-24 10:30:22 -0300

    Feat: Docker aplicado com fallback para desenvolvimento rápido

---
+++ src/main/java/br/edu/fatecgru/mercado_inteligente/controller/TesteController.java
@@ -0,0 +1,15 @@
+package br.edu.fatecgru.mercado_inteligente.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TesteController {
+
+	@GetMapping("/teste")
+	public String teste() {
+		return "API funcionando legal 🚀";
+	}
+}
