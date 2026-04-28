ef63c873b2394b9bac498e2338adfbc56143d965
math-fsilva <matheusmf755887@gmail.com>
2026-04-28 15:59:19 -0300

    Feat: Métodos de busca no Repository

--- src/main/java/br/edu/fatecgru/mercado_inteligente/repository/FornecedorRepository.java
+++ src/main/java/br/edu/fatecgru/mercado_inteligente/repository/FornecedorRepository.java
@@ -1,9 +1,14 @@
 package br.edu.fatecgru.mercado_inteligente.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.edu.fatecgru.mercado_inteligente.model.entity.Fornecedor;
 
 public interface FornecedorRepository extends JpaRepository<Fornecedor, Integer> {
+
+	// Método para fazer buscar por fornecedor
+	public List<Fornecedor> findByNomeContains(String nome);
 
 }
--- src/main/java/br/edu/fatecgru/mercado_inteligente/repository/MarcaRepository.java
+++ src/main/java/br/edu/fatecgru/mercado_inteligente/repository/MarcaRepository.java
@@ -1,9 +1,14 @@
 package br.edu.fatecgru.mercado_inteligente.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.edu.fatecgru.mercado_inteligente.model.entity.Marca;
 
 public interface MarcaRepository extends JpaRepository<Marca, Integer> {
+
+	// Método para fazer buscar por categoria
+	public List<Marca> findByNomeContains(String nome);
 
 }
