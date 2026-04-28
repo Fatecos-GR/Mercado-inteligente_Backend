4faf43cfd6e45cc5fa434386446bf81f5581f800
math-fsilva <matheusmf755887@gmail.com>
2026-04-28 15:58:23 -0300

    Feat: Métodos de busca no Service

--- src/main/java/br/edu/fatecgru/mercado_inteligente/service/CategoriaService.java
+++ src/main/java/br/edu/fatecgru/mercado_inteligente/service/CategoriaService.java
@@ -1,5 +1,32 @@
 package br.edu.fatecgru.mercado_inteligente.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.edu.fatecgru.mercado_inteligente.model.entity.Categoria;
+import br.edu.fatecgru.mercado_inteligente.repository.CategoriaRepository;
+
+@Service
 public class CategoriaService {
+
+	// Método de listar todos
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	public List<Categoria> listarTodos() {
+		return categoriaRepository.findAll();
+	}
+
+	// Consulta por categoria que contém no nome
+	public List<Categoria> getByContainsName(String nome) {
+		return categoriaRepository.findByNomeContains(nome);
+	}
+
+	// Consulta por id categoria
+	public Categoria getById(int id) {
+		return categoriaRepository.findById(id).orElse(null);
+	}
 
 }
--- src/main/java/br/edu/fatecgru/mercado_inteligente/service/FornecedorService.java
+++ src/main/java/br/edu/fatecgru/mercado_inteligente/service/FornecedorService.java
@@ -1,5 +1,32 @@
 package br.edu.fatecgru.mercado_inteligente.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.edu.fatecgru.mercado_inteligente.model.entity.Fornecedor;
+import br.edu.fatecgru.mercado_inteligente.repository.FornecedorRepository;
+
+@Service
 public class FornecedorService {
+
+	// Método para listar todos
+	@Autowired
+	private FornecedorRepository fornecedorRepository;
+
+	public List<Fornecedor> listarTodos() {
+		return fornecedorRepository.findAll();
+	}
+
+	// Listar pelo ID do fornecedor
+	public Fornecedor getById(int id) {
+		return fornecedorRepository.findById(id).orElse(null);
+	}
+
+	// Listar produto pelo o nome "contido"
+	public List<Fornecedor> getByContainsName(String nome) {
+		return fornecedorRepository.findByNomeContains(nome);
+	}
 
 }
