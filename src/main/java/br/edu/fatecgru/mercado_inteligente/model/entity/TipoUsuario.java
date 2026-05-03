3678e8a7ed0edeec2e72917003de90d226cffe28
Matheus Fernandes <matheusmf755887@gmail.com>
2026-05-03 13:04:03 -0300

    Fix: Correção no enumerate dos tipos de usuário

--- src/main/java/br/edu/fatecgru/mercado_inteligente/model/entity/TipoUsuario.java
+++ src/main/java/br/edu/fatecgru/mercado_inteligente/model/entity/TipoUsuario.java
@@ -1,6 +1,6 @@
 package br.edu.fatecgru.mercado_inteligente.model.entity;
 
-//Enum para determinar o tipo do usuário como administrador ou cliente
+// Enum para determinar o tipo do usuário como cliente ou funcionário
 public enum TipoUsuario {
-	ADMIN, CLIENTE
+	CLIENTE, FUNCIONARIO
 }
