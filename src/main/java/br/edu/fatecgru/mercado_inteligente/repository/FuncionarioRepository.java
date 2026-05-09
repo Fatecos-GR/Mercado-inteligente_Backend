cecc2859d25bc1f20ed40dab980ba7b6358de3df
math-fsilva <matheusmf755887@gmail.com>
2026-05-09 03:25:37 -0300

    Feat: Métodos de listar clientes, administradores e estoquistas

--- src/main/java/br/edu/fatecgru/mercado_inteligente/repository/FuncionarioRepository.java
+++ src/main/java/br/edu/fatecgru/mercado_inteligente/repository/FuncionarioRepository.java
@@ -1,9 +1,13 @@
 package br.edu.fatecgru.mercado_inteligente.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.edu.fatecgru.mercado_inteligente.model.entity.Funcionario;
+import br.edu.fatecgru.mercado_inteligente.model.entity.TipoFuncionario;
 
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer> {
+	List<Funcionario> findByTipoFuncionario(TipoFuncionario tipoFuncionario);
 
 }
--- src/main/java/br/edu/fatecgru/mercado_inteligente/service/UsuarioService.java
+++ src/main/java/br/edu/fatecgru/mercado_inteligente/service/UsuarioService.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.edu.fatecgru.mercado_inteligente.model.entity.Funcionario;
+import br.edu.fatecgru.mercado_inteligente.model.entity.TipoFuncionario;
 import br.edu.fatecgru.mercado_inteligente.model.entity.Usuario;
+import br.edu.fatecgru.mercado_inteligente.repository.FuncionarioRepository;
 import br.edu.fatecgru.mercado_inteligente.repository.UsuarioRepository;
 
 @Service
@@ -14,6 +17,9 @@ public class UsuarioService {
 	// Método para listar todos
 	@Autowired
 	private UsuarioRepository usuarioRepository;
+
+	@Autowired
+	private FuncionarioRepository funcionarioRepository;
 
 	public List<Usuario> listarTodos() {
 		return usuarioRepository.findAll();
@@ -27,6 +33,24 @@ public class UsuarioService {
 	// Listar produto pelo o nome "contido"
 	public List<Usuario> getByContainsName(String nome) {
 		return usuarioRepository.findByNomeContains(nome);
+	}
+
+	// Listar clientes
+	public List<Usuario> listarClientes() {
+
+		return usuarioRepository.findAll().stream().filter(usuario -> !(usuario instanceof Funcionario)).toList();
+	}
+
+	// Listar administradores
+	public List<Funcionario> listarAdministradores() {
+
+		return funcionarioRepository.findByTipoFuncionario(TipoFuncionario.ADMIN);
+	}
+
+	// Listar estoquistas
+	public List<Funcionario> listarEstoquistas() {
+
+		return funcionarioRepository.findByTipoFuncionario(TipoFuncionario.ESTOQUISTA);
 	}
 
 }
