988c09105bdcc8c3ac5335e0a624f1c1fb973b1c
Renan Amaral <renanalvarado123@gmail.com>
2026-05-18 21:53:55 -0300

    Fix: Tags para o Swagger

--- src/main/java/br/edu/fatecgru/mercado_inteligente/controller/FornecedorController.java
+++ src/main/java/br/edu/fatecgru/mercado_inteligente/controller/FornecedorController.java
@@ -11,11 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.fatecgru.mercado_inteligente.model.entity.Fornecedor;
 import br.edu.fatecgru.mercado_inteligente.service.FornecedorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @CrossOrigin(origins = "*")
 @RestController
 //Cria o geral, todos precisam desse
 @RequestMapping("/api/fornecedores")
+@Tag(name = "Fornecedores", description = "Endpoints relacionados aos Fornecedores")
 public class FornecedorController {
 
 	@Autowired
@@ -23,20 +26,22 @@ public class FornecedorController {
 
 	// Lista todas os fornecedores
 	@GetMapping
+	@Operation(summary = "Listar todos os fornecedores")
 	public List<Fornecedor> listarTodos() {
 		return fornecedorService.listarTodos();
 	}
 
 	// Busca por ID
 	@GetMapping("/{id}")
+	@Operation(summary = "Buscar fornecedor por ID")
 	public Fornecedor buscarPorId(@PathVariable Long id) {
 		return fornecedorService.getById(id);
 	}
 
 	// Busca de fornecedor pelo nome
 	@GetMapping("/contem-nome/{nome}")
+	@Operation(summary = "Buscar fornecedor por nome")
 	public List<Fornecedor> buscarPorContemNome(@PathVariable String nome) {
 		return fornecedorService.getByContainsName(nome);
 	}
-
 }
--- src/main/java/br/edu/fatecgru/mercado_inteligente/controller/UsuarioController.java
+++ src/main/java/br/edu/fatecgru/mercado_inteligente/controller/UsuarioController.java
@@ -14,10 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 import br.edu.fatecgru.mercado_inteligente.model.entity.Funcionario;
 import br.edu.fatecgru.mercado_inteligente.model.entity.Usuario;
 import br.edu.fatecgru.mercado_inteligente.service.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/usuarios")
+@Tag(name = "Usuários", description = "Endpoints relacionados aos Usuários")
 public class UsuarioController {
 
 	@Autowired
@@ -25,18 +28,21 @@ public class UsuarioController {
 
 	@GetMapping
 	@PreAuthorize("hasRole('ADMIN')")
+	@Operation(summary = "Listar todos os usuários(Apenas ADMIN)")
 	public List<Usuario> listarTodos() {
 		return usuarioService.listarTodos();
 	}
 
 	@GetMapping("/{id}")
 	@PreAuthorize("hasRole('ADMIN')")
+	@Operation(summary = "Listar usuário por ID (Apenas ADMIN)")
 	public Usuario buscarPorId(@PathVariable Long id) {
 		return usuarioService.getById(id);
 	}
 
 	@GetMapping("/contem-nome/{nome}")
 	@PreAuthorize("hasRole('ADMIN')")
+	@Operation(summary = "Listar usuário por Nome (Apenas ADMIN)")
 	public List<Usuario> buscarPorContemNome(@PathVariable String nome) {
 		return usuarioService.getByContainsName(nome);
 	}
@@ -44,6 +50,7 @@ public class UsuarioController {
 	// Método para listar clientes
 	@GetMapping("/clientes")
 	@PreAuthorize("hasRole('ADMIN')")
+	@Operation(summary = "Listar clientes (Apenas ADMIN)")
 	public ResponseEntity<List<Usuario>> listarClientes() {
 
 		return ResponseEntity.ok(usuarioService.listarClientes());
@@ -52,6 +59,7 @@ public class UsuarioController {
 	// Método para listar administradores
 	@GetMapping("/admins")
 	@PreAuthorize("hasRole('ADMIN')")
+	@Operation(summary = "Listar admins (Apenas ADMIN)")
 	public ResponseEntity<List<Funcionario>> listarAdmins() {
 
 		return ResponseEntity.ok(usuarioService.listarAdministradores());
@@ -60,6 +68,7 @@ public class UsuarioController {
 	// Método para listar estoquistas
 	@GetMapping("/estoquistas")
 	@PreAuthorize("hasRole('ADMIN')")
+	@Operation(summary = "Listar estoquistas (Apenas ADMIN)")
 	public ResponseEntity<List<Funcionario>> listarEstoquistas() {
 
 		return ResponseEntity.ok(usuarioService.listarEstoquistas());
