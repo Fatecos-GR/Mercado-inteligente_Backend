4a6c0d21caaf06005e5759a50fb40e4ead7f5242
pedro <pedrohenriquedez12@gmail.com>
2026-05-31 00:18:30 -0300

    implementa exclusão em cascata e integridade de Marcas

--- src/main/java/br/edu/fatecgru/mercado_inteligente/service/MarcaService.java
+++ src/main/java/br/edu/fatecgru/mercado_inteligente/service/MarcaService.java
@@ -4,61 +4,67 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.edu.fatecgru.mercado_inteligente.model.dto.MarcaDTO;
 import br.edu.fatecgru.mercado_inteligente.model.entity.Marca;
+import br.edu.fatecgru.mercado_inteligente.model.entity.Produto;
 import br.edu.fatecgru.mercado_inteligente.repository.MarcaRepository;
+import br.edu.fatecgru.mercado_inteligente.repository.ProdutoRepository;
 
 @Service
 public class MarcaService {
 
-	// Método de listar todos
 	@Autowired
 	private MarcaRepository marcaRepository;
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
+	@Autowired
+	private ProdutoService produtoService;
 
 	public List<Marca> listarTodos() {
 		return marcaRepository.findAll();
 	}
 
-	// Listar por marca que contém no nome
 	public List<Marca> getByContainsName(String nome) {
 		return marcaRepository.findByNomeContains(nome);
 	}
 
-	// Consulta por ID da marca
 	public Marca getById(Long id) {
 		return marcaRepository.findById(id).orElse(null);
 	}
 
-	// salvar
 	public Marca save(Marca marca) {
 		return marcaRepository.save(marca);
 	}
 
-	// cadastrar
 	public Marca cadastrar(MarcaDTO dto) {
-
 		Marca marca = new Marca();
-
 		marca.setNome(dto.getNome());
 		marca.setDescricao(dto.getDescricao());
-
 		return marcaRepository.save(marca);
 	}
 
-	// atualizar
 	public Marca atualizar(Long id, MarcaDTO dto) {
-
 		Marca marca = marcaRepository.findById(id).orElseThrow(() -> new RuntimeException("Marca não encontrada"));
-
 		marca.setNome(dto.getNome());
 		marca.setDescricao(dto.getDescricao());
-
 		return marcaRepository.save(marca);
 	}
 
-	// deletar
+	@Transactional
 	public void delete(Long id) {
+		List<Produto> produtos = produtoRepository.findByMarcaId(id);
+		
+		// 1. Validar se algum produto da marca está em carrinho ativo
+		// O método deleteProduto já faz essa validação interna e lança IllegalStateException
+		for (Produto produto : produtos) {
+			produtoService.deleteProduto(produto.getId());
+		}
+
+		// 2. Se passou por todos os produtos sem erro, deleta a marca
 		marcaRepository.deleteById(id);
 	}
 
