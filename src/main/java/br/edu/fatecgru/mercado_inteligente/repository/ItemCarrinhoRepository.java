2b34f638209c7271273007a5951de2fc38177406
pedro <pedrohenriquedez12@gmail.com>
2026-05-31 00:07:40 -0300

    feat: bloqueio de exclusão de produtos presentes em carrinhos ativos

--- src/main/java/br/edu/fatecgru/mercado_inteligente/repository/ItemCarrinhoRepository.java
+++ src/main/java/br/edu/fatecgru/mercado_inteligente/repository/ItemCarrinhoRepository.java
@@ -5,7 +5,10 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.edu.fatecgru.mercado_inteligente.model.entity.ItemCarrinho;
+import br.edu.fatecgru.mercado_inteligente.model.entity.StatusCarrinho;
 
 public interface ItemCarrinhoRepository extends JpaRepository<ItemCarrinho, Long> {
     Optional<ItemCarrinho> findByCarrinhoIdAndProdutoId(Long carrinhoId, Long produtoId);
+    
+    boolean existsByProdutoIdAndCarrinhoStatus(Long produtoId, StatusCarrinho status);
 }
