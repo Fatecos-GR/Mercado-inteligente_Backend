938a1f62d27531e0dba9766ef404cf619081af09
Matheus Fernandes <matheusmf755887@gmail.com>
2026-06-01 02:19:39 -0300

    Update FornecedorService.java
    Signed-off-by: Matheus Fernandes <matheusmf755887@gmail.com>

--- src/main/java/br/edu/fatecgru/mercado_inteligente/service/FornecedorService.java
+++ src/main/java/br/edu/fatecgru/mercado_inteligente/service/FornecedorService.java
@@ -1,11 +1,7 @@
 package br.edu.fatecgru.mercado_inteligente.service;
 
-import br.edu.fatecgru.mercado_inteligente.model.dto.EnderecoDTO;
-import br.edu.fatecgru.mercado_inteligente.model.dto.FornecedorDTO;
-import br.edu.fatecgru.mercado_inteligente.model.entity.Endereco;
-import br.edu.fatecgru.mercado_inteligente.model.entity.Fornecedor;
-import br.edu.fatecgru.mercado_inteligente.repository.FornecedorRepository;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,56 +14,10 @@ import br.edu.fatecgru.mercado_inteligente.repository.FornecedorRepository;
 
 @Service
 public class FornecedorService {
-  // Método para listar todos
-  @Autowired
-  private FornecedorRepository fornecedorRepository;
 
-  public List<Fornecedor> listarTodos() {
-    return fornecedorRepository.findAll();
-  }
-
-  // Listar pelo ID do fornecedor
-  public Fornecedor getById(Long id) {
-    return fornecedorRepository.findById(id).orElse(null);
-  }
-
-  // Listar produto pelo o nome "contido"
-  public List<Fornecedor> getByContainsName(String nome) {
-    return fornecedorRepository.findByNomeContains(nome);
-  }
-
-  // salvar
-  public Fornecedor save(Fornecedor fornecedor) {
-    return fornecedorRepository.save(fornecedor);
-  }
-
-  // cadastrar
-  public Fornecedor cadastrar(FornecedorDTO dto) {
-    Fornecedor fornecedor = new Fornecedor();
-
-    fornecedor.setNome(dto.nome());
-
-    Endereco endereco = criarEndereco(dto.endereco());
-
-    fornecedor.setEndereco(endereco);
-
-    return fornecedorRepository.save(fornecedor);
-  }
-
-  // atualizar
-  public Fornecedor atualizar(Long id, FornecedorDTO dto) {
-    Fornecedor fornecedor = fornecedorRepository
-      .findById(id)
-      .orElseThrow(() -> new RuntimeException("Fornecedor não encontrado"));
-
-    fornecedor.setNome(dto.nome());
-
-    Endereco endereco = criarEndereco(dto.endereco());
-
-    fornecedor.setEndereco(endereco);
-
-    return fornecedorRepository.save(fornecedor);
-  }
+	// Método para listar todos
+	@Autowired
+	private FornecedorRepository fornecedorRepository;
 
 	@Autowired
 	private ImagemService imagemService;
@@ -76,17 +26,15 @@ public class FornecedorService {
 		return fornecedorRepository.findAll();
 	}
 
-  // método auxiliar
-  private Endereco criarEndereco(EnderecoDTO dto) {
-    Endereco endereco = new Endereco();
+	// Listar pelo ID do fornecedor
+	public Fornecedor getById(Long id) {
+		return fornecedorRepository.findById(id).orElse(null);
+	}
 
-    endereco.setCep(dto.cep());
-    endereco.setLogradouro(dto.logradouro());
-    endereco.setNumero(dto.numero());
-    endereco.setComplemento(dto.complemento());
-    endereco.setBairro(dto.bairro());
-    endereco.setCidade(dto.cidade());
-    endereco.setEstado(dto.estado());
+	// Listar produto pelo o nome "contido"
+	public List<Fornecedor> getByContainsName(String nome) {
+		return fornecedorRepository.findByNomeContains(nome);
+	}
 
 	// salvar
 	public Fornecedor save(Fornecedor fornecedor) {
