d14361dc42a799190778aa61500eae29d5bb6daa
Matheus Fernandes <matheusmf755887@gmail.com>
2026-06-16 00:55:02 -0300

    Response (#54)
    * Feat: Cloudinary
    
    * Merge branch 'development' of
    https://github.com/Fatecos-GR/Mercado-inteligente_Backend.git into
    development

--- src/main/java/br/edu/fatecgru/mercado_inteligente/service/AuthService.java
+++ src/main/java/br/edu/fatecgru/mercado_inteligente/service/AuthService.java
@@ -16,6 +16,7 @@ import br.edu.fatecgru.mercado_inteligente.exception.EmailJaCadastradoException;
 import br.edu.fatecgru.mercado_inteligente.model.dto.LoginRequest;
 import br.edu.fatecgru.mercado_inteligente.model.dto.LoginResponse;
 import br.edu.fatecgru.mercado_inteligente.model.dto.RegistroRequest;
+import br.edu.fatecgru.mercado_inteligente.model.dto.RegistroResponse;
 import br.edu.fatecgru.mercado_inteligente.model.dto.UsuarioLogadoDTO;
 import br.edu.fatecgru.mercado_inteligente.model.entity.Usuario;
 import br.edu.fatecgru.mercado_inteligente.repository.UsuarioRepository;
@@ -44,7 +45,7 @@ public class AuthService {
 	private static final Logger log = LoggerFactory.getLogger(AuthService.class);
 
 	// Registro de novo usuário
-	public void registrar(RegistroRequest request) {
+	public RegistroResponse registrar(RegistroRequest request) {
 
 		// Validação
 		if (usuarioRepository.existsByEmail(request.email())) {
@@ -61,7 +62,11 @@ public class AuthService {
 		// Senha criptografada
 		novoUsuario.setSenha(passwordEncoder.encode(request.senha()));
 
-		usuarioRepository.save(novoUsuario);
+		Usuario usuarioSalvo = usuarioRepository.save(novoUsuario);
+
+		return new RegistroResponse(usuarioSalvo.getId(), usuarioSalvo.getNome(), usuarioSalvo.getSobrenome(),
+				usuarioSalvo.getEmail(), usuarioSalvo.getTelefone());
+
 	}
 
 	// Login do usuário
