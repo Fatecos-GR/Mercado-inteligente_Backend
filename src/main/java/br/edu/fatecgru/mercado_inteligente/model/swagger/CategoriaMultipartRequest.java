4155ad0bce6b6b0723cfe4a18e2830e49c3cfd48
math-fsilva <matheusmf755887@gmail.com>
2026-06-16 18:53:44 -0300

    Fix: Request de Categoria

--- src/main/java/br/edu/fatecgru/mercado_inteligente/model/swagger/CategoriaMultipartRequest.java
+++ src/main/java/br/edu/fatecgru/mercado_inteligente/model/swagger/CategoriaMultipartRequest.java
@@ -1,5 +1,32 @@
 package br.edu.fatecgru.mercado_inteligente.model.swagger;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import br.edu.fatecgru.mercado_inteligente.model.dto.CategoriaDTO;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(name = "CategoriaMultipartRequest")
 public class CategoriaMultipartRequest {
 
+	@Schema(description = "Dados da categoria", implementation = CategoriaDTO.class)
+	private CategoriaDTO categoria;
+
+	@Schema(description = "Imagem da categoria", type = "string", format = "binary")
+	private MultipartFile imagem;
+
+	public CategoriaDTO getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(CategoriaDTO categoria) {
+		this.categoria = categoria;
+	}
+
+	public MultipartFile getImagem() {
+		return imagem;
+	}
+
+	public void setImagem(MultipartFile imagem) {
+		this.imagem = imagem;
+	}
 }
